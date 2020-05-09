e3c20578aa140c7157a8f4fe4f7cdf94c46e3fae
artemkoltsa <artemkoltsa@gmail.com>
2020-05-10 00:08:56 +0300

    update

--- application.py
+++ application.py
@@ -1,10 +1,3 @@
-# from flask import Flask
-# app = Flask(__name__)
-
-# @app.route("/")
-# def hello():
-#     return "Hello World!"
-
 # coding: utf-8
 # Импортирует поддержку UTF-8.
 from __future__ import unicode_literals
@@ -58,36 +51,32 @@ def handle_dialog(req, res):
 
         sessionStorage[user_id] = {
             'suggests': [
-                "Я чувствую жар",
-                "Мне жарко",
-                "У меня температура",
-                "Меня знобит",
+                "Не хочу.",
+                "Не буду.",
+                "Отстань!",
             ]
         }
 
-        res['response']['text'] = 'Здравствуйте, это первая медицинская помощь от Алисы. Я объясню Вам принципы оказания первой помощи. Чем я могу Вам помочь?'
+        res['response']['text'] = 'Привет! Купи слона!'
         res['response']['buttons'] = get_suggests(user_id)
         return
 
     # Обрабатываем ответ пользователя.
     if req['request']['original_utterance'].lower() in [
-        'нет',
-        'пока',
-        'нет, спасибо',
+        'ладно',
+        'куплю',
+        'покупаю',
+        'хорошо',
     ]:
         # Пользователь согласился, прощаемся.
-        res['response']['text'] = 'Спасибо, что обратились ко мне за первой помощью!'
+        res['response']['text'] = 'Слона можно найти на Яндекс.Маркете!'
         return
 
-    res['response']['text'] = 'Все говорят "%s", а ты купи слона!'
-    res['response']['buttons'] = get_suggests(user_id)
-
-
     # Если нет, то убеждаем его купить слона!
-    # res['response']['text'] = 'Все говорят "%s", а ты купи слона!' % (
-    #     req['request']['original_utterance']
-    # )
-    # res['response']['buttons'] = get_suggests(user_id)
+    res['response']['text'] = 'Все говорят "%s", а ты купи слона!' % (
+        req['request']['original_utterance']
+    )
+    res['response']['buttons'] = get_suggests(user_id)
 
 # Функция возвращает две подсказки для ответа.
 def get_suggests(user_id):
