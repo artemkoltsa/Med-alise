d627adb910f09f044955d38a80a72c1c8197cb5f
artemkoltsa <artemkoltsa@gmail.com>
2020-05-27 00:32:33 +0300

    update

--- application.py
+++ application.py
@@ -88,16 +88,16 @@ def handle_dialog(req, res):
 
         no_no = [ {'title': "Нет", 'hide': True} ]
 
-        res['response']['text'] = 'т, это первая медицинская помощь от Алисы. Я объясню Вам принципы оказания первой помощи. Чем я могу Вам помочь?'
+        res['response']['text'] = ', это первая медицинская помощь от Алисы. Я объясню Вам принципы оказания первой помощи. Чем я могу Вам помочь?'
         res['response']['buttons'] = choice_buttons
         return
 
-    if req['request']['original_utterance'] == 'нет':
+    if req['request']['original_utterance'] == 'Нет':
         res['response']['text'] = 'Спасибо, что обратились ко мне за первой помощью!'
         res['response']['end_session'] = True
         return
 
-    if req['request']['original_utterance'] == ['я чувствую жар', 'мне жарко']:
+    if req['request']['original_utterance'] == ['Я чувствую жар', 'Мне жарко']:
         res['response']['text'] = 'Померьте температуру и напишите в градусах Цельсия через запятую. Пример: "36,6".'
         res['response']['buttons'] = temp_buttons
         return
